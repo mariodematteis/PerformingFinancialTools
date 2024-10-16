2a14aa81833956cb91149f29f8d3efd6e5bd63bd
BopaxDev <marionicdematteis@gmail.com>
2024-10-16 16:23:58 -0400

    Update header file

--- include/Security.hpp
+++ include/Security.hpp
@@ -10,8 +10,8 @@ namespace PFT::Security
                                                std::is_same_v<T, double> ||
                                                std::is_same_v<T, std::string>;
 
-    static const std::string DEFAULT_SYMBOL = "undefined";
-    static const std::string DEFAULT_NAME = "undefined";
+    static const std::string default_symbol = "undefined";
+    static const std::string default_name = "undefined";
 
     /*
     Security generally defines any types of tradable financial instruments.
@@ -30,14 +30,14 @@ namespace PFT::Security
         double asset_price_ = 0.0;
 
     public:
-        auto get_symbol() const -> const std::string& { return symbol_; }
-        auto get_name() const -> const std::string& { return name_; }
-        auto get_asset_class() const -> const std::string& { return asset_class_; }
+        auto get_symbol() const -> const std::string & { return symbol_; }
+        auto get_name() const -> const std::string & { return name_; }
+        auto get_asset_class() const -> const std::string & { return asset_class_; }
         auto get_asset_price() const -> double { return asset_price_; }
 
-        auto set_asset_class(const std::string& asset_class) -> void { asset_class_ = asset_class; }
-        auto set_symbol(const std::string& symbol) -> void { symbol_ = symbol; }
-        auto set_name(const std::string& name) -> void { name_ = name; }
+        auto set_asset_class(const std::string &asset_class) -> void { asset_class_ = asset_class; }
+        auto set_symbol(const std::string &symbol) -> void { symbol_ = symbol; }
+        auto set_name(const std::string &name) -> void { name_ = name; }
         auto set_asset_price(double asset_price) -> void { asset_price_ = asset_price; }
     };
 }
