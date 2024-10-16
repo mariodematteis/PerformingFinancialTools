3421a40e48969c7f6795108d024d7d6ed83647b5
BopaxDev <marionicdematteis@gmail.com>
2024-10-16 14:30:08 -0400

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,18 @@
-#include <Derivative/Derivative.hpp>
 #include "FixedIncome/Bond.hpp"
 
-using namespace PFT::Security::FixedIncome;
+#include <iostream>
 
 int main()
 {
-    Bond bond(2,
-              0.02,
-              100,
-              0.06,
-              2);
+    PFT::Security::FixedIncome::Annuity ann{500, 0.04, 2, 10};
+    std::cout << "Current Price -> " << ann.current_price() << std::endl;
 
-    bond.price();
+    PFT::Security::FixedIncome::Perpetuity perp{500, 0.04};
+    std::cout << "Current Price -> " << perp.current_price() << std::endl;
 
-    return 0;
+    PFT::Security::FixedIncome::ZeroCouponBond zcb{100, 0.04, 2, 1};
+    std::cout << "Current Price -> " << zcb.current_price() << std::endl;
+
+    PFT::Security::FixedIncome::CouponBond coupon_bond{100, 0.04, 0.02, 2, 10};
+    std::cout << "Current Price -> " << coupon_bond.get_asset_price() << std::endl;
 }
