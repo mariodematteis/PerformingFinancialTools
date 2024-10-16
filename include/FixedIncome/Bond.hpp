6472f5c6e9f9aea75f9daeacd4758875d11a74ea
BopaxDev <marionicdematteis@gmail.com>
2024-10-16 14:27:41 -0400

    Update header files

--- include/FixedIncome/Bond.hpp
+++ include/FixedIncome/Bond.hpp
@@ -1,8 +1,10 @@
 #pragma once
 
-#include "Asset.hpp"
+#include "Security.hpp"
 #include "../Tools.hpp"
 
+#include <array>
+#include <algorithm>
 #include <format>
 #include <vector>
 #include <numeric>
@@ -12,7 +14,11 @@
 
 namespace PFT::Security::FixedIncome
 {
-    static const std::string BOND_ASSET_CLASS = "bond";
+    static const std::string bond_security_default_class = "bond";
+    static const std::string annuity_security_default_class = "annuity";
+    static const std::string perpetuity_security_default_class = "perpetuity";
+    static const std::string zcb_security_default_class = "zero_coupon_bond";
+    static const std::string coupon_bond_security_default_class = "coupon_bond";
 
     static const std::string MATURITY_KEY = "maturity";
     static const std::string COUPON_RATE_KEY = "coupon_rate";
@@ -22,21 +28,123 @@ namespace PFT::Security::FixedIncome
 
     static const std::string_view FORMAT_BOND = "Bond Sheet Information\n\nMaturity -> \t {}\nFace Value -> \t {}";
 
-    class Bond : public Asset
+    class CashFlows
+    {
+    private:
+        std::unordered_map<double, double> cash_flows_scheme;
+
+    public:
+        CashFlows();
+        CashFlows(const std::unordered_map<double, double> &cf);
+
+        double operator()(double annual_interest_rate,
+                          int compounding_frequency,
+                          double maturity = 0.0) const
+        {
+            double power = compounding_frequency * maturity;
+            if (!maturity)
+                power = 1.0;
+
+            return std::pow((1 / (1 + annual_interest_rate / compounding_frequency)), power);
+        }
+
+        double current_price() const;
+        int add_cash_flow(
+            double maturity,
+            double amount,
+            bool override = false);
+
+        void describe() const;
+        std::unique_ptr<std::unordered_map<double, double>> get_cash_flows_scheme()
+        {
+            return std::make_unique<std::unordered_map<double, double>>(cash_flows_scheme);
+        }
+    };
+
+    class Annuity : public Security, public CashFlows
+    {
+    private:
+        const double amount_;
+        const double interest_rate_;
+        const int compounding_frequency_;
+        const double maturity_;
+
+    public:
+        Annuity(double amount,
+                double interest_rate,
+                int compounding_frequency,
+                double maturity,
+                const std::string &symbol = DEFAULT_SYMBOL,
+                const std::string &name = DEFAULT_NAME);
+    };
+
+    class Perpetuity : public Security
+    {
+    private:
+        double amount_;
+        double interest_rate_;
+
+    public:
+        Perpetuity(double amount,
+                   double interest_rate);
+
+        double current_price();
+    };
+
+    class ZeroCouponBond : public Security, public CashFlows
+    {
+    private:
+        const double face_value_;
+        const double interest_rate_;
+        const int compounding_frequency_;
+        const double maturity_;
+
+    public:
+        ZeroCouponBond(double face_value,
+                       double interest_rate,
+                       int compounding_frequency,
+                       double maturity,
+                       const std::string &symbol = DEFAULT_SYMBOL,
+                       const std::string &name = DEFAULT_NAME);
+    };
+
+    class CouponBond : public Security
+    {
+    private:
+        const double face_value_;
+        const double interest_rate_;
+        const double coupon_rate_;
+        const int compounding_frequency_;
+        const double maturity_;
+        const double coupon_amount_;
+
+        std::unique_ptr<Annuity> annuity_;
+        std::unique_ptr<ZeroCouponBond> zcb_;
+
+    public:
+        CouponBond(double face_value,
+                   double interest_rate,
+                   double coupon_rate,
+                   int compounding_frequency,
+                   double maturity,
+                   const std::string &symbol = DEFAULT_SYMBOL,
+                   const std::string &name = DEFAULT_NAME);
+    };
+
+    class Bond : public Security
     {
 
     private:
         int coupon_year_frequency = 0;
-        double coupon_rate = 0.0f;
-        double face_value = 100.0f;
-        double interest_rate = 0.0f;
-        double maturity = 1.0f;
+        double coupon_rate = 0.0;
+        double face_value = 100.0;
+        double interest_rate = 0.0;
+        double maturity = 1.0;
 
-        double coupon_payment = 0.0f;
-        double maturity_payment = 0.0f;
+        double coupon_payment = 0.0;
+        double maturity_payment = 0.0;
         double ytm = 0.0;
-        std::shared_ptr<std::vector<double>>
-            discounts;
+        std::shared_ptr<std::vector<double>> discounts;
 
     public:
         template <PFT::Tools::AllowedTypes T>
@@ -45,28 +153,28 @@ namespace PFT::Security::FixedIncome
              T face_value,
              T coupon_rate,
              int coupon_year_frequency,
-             std::string symbol = DEFAULT_SYMBOL,
-             std::string name = DEFAULT_NAME);
+             const std::string &symbol = DEFAULT_SYMBOL,
+             const std::string &name = DEFAULT_NAME);
 
-        template <AllowedSecuritySpecificationType T>
-        Bond(std::unordered_map<std::string, T> specification);
-        Bond(std::string filename);
+        template <PFT::Security::AllowedSecuritySpecificationType T>
+        Bond(const std::unordered_map<std::string, T> &specification);
+        Bond(const std::string &filename);
+
+        virtual double price() const { return 0.0; }
+        virtual double yield_to_maturity() const { return 0.0; }
 
         virtual ~Bond() = default;
 
-        auto get_maturity() const -> double;
-        auto get_interest_rate() const -> double;
-        auto get_face_value() const -> double;
-        auto get_coupon_rate() const -> double;
-        auto get_coupon_year_frequency() const -> int;
-        auto get_maturity_payment() const -> double;
-        auto get_coupon_payment() const -> double;
-        auto get_discounts() const -> std::shared_ptr<std::vector<double>>;
+        double get_maturity() const;
+        double get_interest_rate() const;
+        double get_face_value() const;
+        double get_coupon_rate() const;
+        int get_coupon_year_frequency() const;
+        double get_maturity_payment() const;
+        double get_coupon_payment() const;
+        std::shared_ptr<std::vector<double>> get_discounts() const;
 
-        virtual auto price() -> double;
-        virtual auto yield_to_maturity() -> double;
-
-        std::string to_string() const override;
-        std::unique_ptr<Asset> clone() const override;
+        std::string to_string() const;
+        std::unique_ptr<Security> clone() const;
     };
 }
--- include/FixedIncome/USBond.hpp
+++ include/FixedIncome/USBond.hpp
@@ -20,6 +20,9 @@ namespace PFT::Security::FixedIncome::USBond
                      std::string auction_date, std::string issue_date, std::string cusip,
                      std::string symbol = DEFAULT_SYMBOL, std::string name = DEFAULT_NAME);
 
+        double price() const override;
+        double yield_to_maturity() const override;
+
         std::string get_auction_date() const
         {
             return auction_date;
@@ -36,16 +39,22 @@ namespace PFT::Security::FixedIncome::USBond
         std::string cusip;
 
     public:
-        TreasuryBill(double maturity, double face_value, double discount_rate,
-                     std::string issue_date, std::string cusip,
+        TreasuryBill(double maturity,
+                     double face_value,
+                     double discount_rate,
+                     std::string issue_date,
+                     std::string cusip,
                      std::string symbol = DEFAULT_SYMBOL, std::string name = DEFAULT_NAME);
+
+        double price() const override;
+        double yield_to_maturity() const override;
 
         double get_discount_rate() const;
         std::string get_issue_date() const;
         std::string get_cusip() const;
 
-        std::string to_string() const override;
-        std::unique_ptr<Asset> clone() const override;
+        std::string to_string() const;
+        std::unique_ptr<Security> clone() const;
     };
 
     class TreasuryNote : public Bond
@@ -58,15 +67,21 @@ namespace PFT::Security::FixedIncome::USBond
         TreasuryNote(double maturity,
                      double interest_rate,
                      double face_value,
-                     double coupon_rate, int coupon_year_frequency,
-                     std::string issue_date, std::string cusip,
-                     std::string symbol = DEFAULT_SYMBOL, std::string name = DEFAULT_NAME);
+                     double coupon_rate,
+                     int coupon_year_frequency,
+                     std::string issue_date,
+                     std::string cusip,
+                     std::string symbol = DEFAULT_SYMBOL,
+                     std::string name = DEFAULT_NAME);
+
+        double price() const override;
+        double yield_to_maturity() const override;
 
         std::string get_issue_date() const;
         std::string get_cusip() const;
 
-        std::string to_string() const override;
-        std::unique_ptr<Asset> clone() const override;
+        std::string to_string() const;
+        std::unique_ptr<Security> clone() const;
     };
 
     class TIPS : public Bond
@@ -76,18 +91,26 @@ namespace PFT::Security::FixedIncome::USBond
         std::vector<double> inflation_adjustments;
 
     public:
-        TIPS(double maturity, double interest_rate, double face_value,
-             double coupon_rate, int coupon_year_frequency,
+        TIPS(double maturity,
+             double interest_rate,
+             double face_value,
+             double coupon_rate,
+             int coupon_year_frequency,
              double inflation_rate,
-             std::string symbol = DEFAULT_SYMBOL, std::string name = DEFAULT_NAME);
+             std::string symbol = DEFAULT_SYMBOL,
+             std::string name = DEFAULT_NAME);
+
+        double price() const override;
+        double yield_to_maturity() const override;
 
         double get_inflation_rate() const;
-        void set_inflation_rate(double new_rate);
         double get_adjusted_face_value() const;
         double get_adjusted_coupon_payment() const;
 
-        std::string to_string() const override;
-        std::unique_ptr<Asset> clone() const override;
+        void set_inflation_rate(double new_rate);
+
+        std::string to_string() const;
+        std::unique_ptr<Security> clone() const;
     };
 
     class MunicipalBond : public Bond
@@ -97,18 +120,25 @@ namespace PFT::Security::FixedIncome::USBond
         std::string state;
 
     public:
-        MunicipalBond(double maturity, double interest_rate, double face_value,
-                      double coupon_rate, int coupon_year_frequency,
-                      bool tax_exempt, std::string state,
-                      std::string symbol = DEFAULT_SYMBOL, std::string name = DEFAULT_NAME);
+        MunicipalBond(double maturity,
+                      double interest_rate,
+                      double face_value,
+                      double coupon_rate,
+                      int coupon_year_frequency,
+                      bool tax_exempt,
+                      std::string state,
+                      std::string symbol = DEFAULT_SYMBOL,
+                      std::string name = DEFAULT_NAME);
+
+        double price() const override;
+        double yield_to_maturity() const override;
 
         bool is_tax_exempt() const;
         std::string get_state() const;
 
-        auto calculate_taxable_equivalent_yield(double tax_rate) const -> double;
+        double calculate_taxable_equivalent_yield(double tax_rate) const;
 
-        std::string to_string() const override;
-        std::unique_ptr<Asset> clone() const override;
+        std::string to_string() const;
+        std::unique_ptr<Security> clone() const;
     };
-
 }
