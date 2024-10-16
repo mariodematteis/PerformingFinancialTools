73c4908485f1003ec2f7a600eeecbdb5ddbab43a
BopaxDev <marionicdematteis@gmail.com>
2024-10-16 14:28:25 -0400

    Update Tools.hpp

--- include/Tools.hpp
+++ include/Tools.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <cstdlib>
 #include <cmath>
 
 namespace PFT::Tools
@@ -13,15 +14,20 @@ namespace PFT::Tools
     {
     public:
         template <AllowedTypes T>
-        auto operator()(T underlying_price,
-                        T strike_price,
-                        T interest_rate,
-                        T maturity,
-                        T current_time,
-                        T sigma,
-                        T dividend_yield) const -> double
+        auto operator()(
+            const T &underlying_price,
+            const T &strike_price,
+            const T &interest_rate,
+            const T &maturity,
+            const T &current_time,
+            const T &sigma,
+            const T &dividend_yield) const -> double
         {
-            return (std::log(underlying_price / strike_price) + (interest_rate - dividend_yield + (pow(sigma, 2.0) / 2)) * (maturity - current_time)) / (sigma * (std::sqrt(maturity - current_time)));
+            return (
+                       std::log(underlying_price / strike_price) +
+                       (interest_rate - dividend_yield + (pow(sigma, 2.0) / 2)) *
+                           (maturity - current_time)) /
+                   (sigma * (std::sqrt(maturity - current_time)));
         }
     };
 
@@ -29,15 +35,21 @@ namespace PFT::Tools
     {
     public:
         template <AllowedTypes T>
-        auto operator()(T underlying_price,
-                        T strike_price,
-                        T interest_rate,
-                        T maturity,
-                        T current_time,
-                        T sigma,
-                        T dividend_yield) const -> double
+        auto operator()(
+            const T &underlying_price,
+            const T &strike_price,
+            const T &interest_rate,
+            T maturity,
+            T current_time,
+            T sigma,
+            T dividend_yield) const -> double
         {
-            return (std::log(underlying_price / strike_price) + (interest_rate - dividend_yield + (pow(sigma, 2.0) / 2)) * (maturity - current_time)) / (sigma * (std::sqrt(maturity - current_time))) - sigma * std::sqrt(maturity - current_time);
+            return (
+                       std::log(underlying_price / strike_price) +
+                       (interest_rate - dividend_yield + (pow(sigma, 2.0) / 2)) *
+                           (maturity - current_time)) /
+                       (sigma * (std::sqrt(maturity - current_time))) -
+                   sigma * std::sqrt(maturity - current_time);
         }
     };
 
